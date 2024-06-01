82f5cddeacda8724c830f9f97238a1592dd056e0
Ezekiel Archibong <112735477+ezekielarchibong@users.noreply.github.com>
2024-06-01 21:27:48 +0100

    ne

--- cityticket/option.js
+++ cityticket/option.js
@@ -1,8 +1,6 @@
-let a = document.getElementById("ticketmaster-option").value;
-function action() {
-  if (a) {
-    window.open(
-      "http://127.0.0.1:5500/project/mattrife/cityticket/contact.html"
-    );
-  }
-}
+// let a = document.getElementById("ticketmaster-option").value;
+// function action() {
+//   if (a) {
+//     window.open("");
+//   }
+// }
